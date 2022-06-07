8df21f97bc5ad0286202807e1a31c2b33918f781
Adriane Caroline <adrianecaroline23@gmail.com>
2022-06-06 22:48:30 -0300

    desafio-start

---
+++ código-js/teste.js
@@ -0,0 +1,63 @@
+function capturar() {
+    let valor = document.getElementById("valor").value;
+    
+    let resultado = valor.split(",",3);
+    console.log(resultado)
+
+    let var1 = parseInt(resultado[0]);
+    let var2 = parseInt(resultado[1]);
+    let var3 = parseInt(resultado[2]);
+
+    let caminhoimagem = climaHumor(var1, var2, var3);
+
+    adcImagem(caminhoimagem);
+
+    //retornar essa funçao dentro do id valor digitado?
+}
+
+function adcImagem(caminhoimagem){
+
+    var img = document.createElement('img');
+    img.src = caminhoimagem;
+    document.getElementById("valorDigitado").appendChild(img);
+    
+}
+
+// essa função ira receber as 3 temperaturas correspondente aos 3 dias. e retornará o caminho da img correspondente ao humor do dia
+function climaHumor(value1, value2, value3) {
+
+    let comparacao = (value1 > value2);
+    let comparacao1 = (value2 <= value3);
+    let comparacao2 = (value1 < value2);
+    let comparacao3 = (value2 >= value3);
+    let comparacao4 = (value1 === value2);
+    let comparacao5 = (value2 < value3);
+    let comparacao6 = (value2 > value3);
+    let subtracao = (value2 - value1);
+    let subtracao1 = (value3 - value2);
+    let subtracao2 = (value1 - value2);
+    let subtracao3 = (value2 - value3);
+    
+    switch (true){
+        case comparacao && comparacao1:
+            return 'assets/c1.png';
+        case comparacao2 && comparacao3:
+            return 'assets/c2.png';
+        case (comparacao2 && comparacao5) && (subtracao > subtracao1):
+            return 'assets/c3.png';
+        case (comparacao2 && comparacao5) && (subtracao < subtracao1):
+            return 'assets/c4.png';
+        case (comparacao && comparacao6) && (subtracao2 > subtracao3):
+            return 'assets/c5.png';
+        case (comparacao && comparacao6) && (subtracao2 == subtracao3):
+            return 'assets/c6.png';
+        case (comparacao4 && comparacao5):
+            return 'assets/c7.png';
+        case (comparacao4 && comparacao6):
+            return 'assets/c8.png';
+        default:
+            return 'assets/c9.png';
+    }
+
+}
+climaHumor(26, 22, 21)
